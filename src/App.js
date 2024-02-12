6bf960678ab4eeab555c0f9526c295fd3ba6a371
TseleMolelekoa <molelekoa58@gmail.com>
2024-02-12 12:01:49 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header/index';
 
-import Header from './components/Header/Header';
-
 
 function App() {
   return (
