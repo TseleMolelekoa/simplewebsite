516e106960526545a3fde5346af5d3101c1a8e23
TseleMolelekoa <molelekoa58@gmail.com>
2024-02-14 11:20:35 +0200

    about page content

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import Header from '../Header';
 import Circle from '../circle'; // Corrected import statement
-import "./about.css";
+import profileImage from '../Assets/assets/_MG_1509_1.jpg'; // Import your profile image
+import './about.css'; // Import your CSS file
 
 const AboutMe = () => {
   return (
     <div>
-    <h2>About Tsele Molelekoa<br/>A Junior Software-Developer</h2>
-      <Header /> 
-    
-      <Circle /> {/* Corrected component name */}
-        <p>
-          Hi! I'm Tsele Molelekoa. I'm passionate about Software Development. 
-          This is a space where I'll share a little bit about myself.<br/><br/>
-       
-          I am a qualified IT enthusiast with experience in providing excellent customer support to network users as well as expertise in troubleshooting technical issues. I have practical experience and competence in a variety of security, networking, and operating system software. <br/>I graduated with a Btech degree in Project Management at the Central University of Technology because I saw the growing need to broaden industry knowledge and skills relating to the management and execution of IT-related projects that any organization wishes to undertake. I am seeking a new challenge in a junior role in IT/project management. I am willing to relocate.
-        </p>
+      <Circle />
+      <Header />
+      
+      <div className="about-me-container">
+        <h2 className="about-heading">About Tsele Molelekoa<br/>A Junior Software-Developer</h2>
+        <div className="content-container">
+          <div className="text">
+            <p className='about_me'>
+              Hi! I'm Tsele Molelekoa. I'm passionate about Software Development. This is a space where I'll share a little bit about myself.
+              <span>I am a qualified IT enthusiast with experience in providing excellent customer support to network users as well as expertise in troubleshooting technical issues.</span>
+              <span> I have practical experience and competence in a variety of security, networking, and operating system software. <br/>I graduated with a Btech degree in Project Management at the Central University of Technology because I saw the growing need to broaden industry knowledge and skills relating to the management and execution of IT-related projects that any organization wishes to undertake. I am seeking a new challenge in a junior role in IT/project management. I am willing to relocate.</span>
+            </p>
+          </div>
+          <div className="image">
+            <img src={profileImage} alt="Profile" />
+          </div>
+        </div>
+        </div>
     </div>
   );
 }
